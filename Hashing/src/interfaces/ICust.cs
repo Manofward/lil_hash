d9ab6a6f41ff1ef0b30137cf3eff8ce58aaacee2
Manofward <janikmeyer1607@outlook.de>
2024-12-20 07:23:23 +0100

    little edit to use it on the HashDB GIt Repo

--- Hashing/src/interfaces/ICust.cs
+++ Hashing/src/interfaces/ICust.cs
@@ -6,5 +6,7 @@ namespace Hashing.src.interfaces
     {
         bool Verify(string input, string hash);
         string Hash(string input, string salt, int saltinc);
+
+        string Salt(int saltLength);
     }
 }
